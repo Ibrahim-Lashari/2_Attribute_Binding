c144f5b6d8240d50a6179a5a0a24b80cb0c6c08b
Ibrahium-Lashari <524Ilashari@frhsd.com>
2024-04-29 14:43:35 -0400

    new one

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
   data () {
     return {
-      product: 'Socks'
+      product: 'turtle',
+      image: '/assets/images/socks_blue.jpg',
+      url: 'https://www.amazon.com/Funny-Novelty-Socks-Stocking-Teenagers/dp/B07Z8HF6VL'
     }
   }
 })
